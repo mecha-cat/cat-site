c5d847199717b477b5b841e7e197a899d5f555b8
Cat-a-clysm <38079631+mecha-cat@users.noreply.github.com>
2023-12-12 19:30:59 +0100

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://cataclysm.systems",
+  compressHTML: false,
+  output: "static",
+  trailingSlash: "never",
+  build: {
+    format: "file",
+    assets: "assets/_astro",
+    inlineStylesheets: "never"
+  }
+});
